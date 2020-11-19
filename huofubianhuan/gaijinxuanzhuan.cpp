20e73b7087702f7fb920b47da8e85ca3684286d1
SJY-2000 <2254789143@qq.com>
2020-11-19 22:18:10 +0800

    +cpp

---
+++ huofubianhuan/gaijinxuanzhuan.cpp
@@ -0,0 +1,21 @@
+//#include<opencv2/opencv.hpp>
+//#include<iostream>
+//using namespace cv;
+//int main()
+//{
+//	Mat srcMat = imread("G:\\picture\\44.jpg");
+//	Mat dstMat;
+//	if (srcMat.empty())return -1;
+//	float angle = -10.0, scale = 1;
+//	Point2f center(srcMat.cols / 2.0, srcMat.rows / 2.0);
+//	Mat rot = getRotationMatrix2D(center, angle, scale);
+//	Rect bbox = RotatedRect(center, srcMat.size(), angle).boundingRect();
+//	rot.at<double>(0, 2) += bbox.width / 2.0 - center.x;
+//	rot.at<double>(1, 2) += bbox.height / 2.0 - center.y;
+//	warpAffine(srcMat, dstMat, rot, bbox.size());
+//
+//	
+//	imshow("src", srcMat);
+//	imshow("dst", dstMat);
+//	waitKey(0);
+//}
---
+++ huofubianhuan/huofuxianduan.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat dstMat;
+	Mat srcMat = imread("G:\\picture\\18.jpg", 1);
+	Mat buffMat = imread("G:\\picture\\18.jpg", 0);
+	Mat lineMat;
+
+	
+	Canny(buffMat, dstMat, 20, 200, 3, false);
+	
+	HoughLinesP(dstMat, lineMat, 1, CV_PI / 180, 15, 40, 5);
+
+	
+	for (int i = 0; i < lineMat.rows; i++)
+	{
+		Point pt1, pt2;
+
+		pt1.x = lineMat.at<Vec4i>(i, 0)[0];
+		pt1.y = lineMat.at<Vec4i>(i, 0)[1];
+		pt2.x = lineMat.at<Vec4i>(i, 0)[2];
+		pt2.y = lineMat.at<Vec4i>(i, 0)[3];
+
+		line(srcMat, pt1, pt2, Scalar(0, 255, 255), 1, LINE_AA);
+	}
+	imshow("srcMat", srcMat);
+	
+	waitKey(0);
+}
