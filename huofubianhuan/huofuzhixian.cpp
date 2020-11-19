a0095fd76a30427cc57d5096a20d34bed9d68631
SJY-2000 <2254789143@qq.com>
2020-11-19 22:18:56 +0800

    +cpp

--- huofubianhuan/huofuzhixian.cpp
+++ huofubianhuan/huofuzhixian.cpp
@@ -1,32 +1,32 @@
-//#include<opencv2/opencv.hpp>
-//#include<iostream>
-//using namespace cv;
-//using namespace std;
-//int main()
-//{
-//	Mat srcMat = imread("G:\\picture\\18.jpg",0);
-//	if (srcMat.empty())return -1;
-//	Mat dstMat;
-//	
-//	std::vector<cv::Vec2f>lines;
-//	Canny(srcMat, dstMat, 20, 230, 3, false);
-//	
-//	HoughLines(dstMat, lines,1,CV_PI/180,100);
-//	std::vector<cv::Vec2f>::iterator it = lines.begin();
-//	for (; it != lines.end(); ++it) {
-//		float rho = (*it)[0], theta = (*it)[1];
-//		Point pt1, pt2;
-//		double a = cos(theta);
-//		double b = sin(theta);
-//		double x0 = a*rho;
-//		double y0 = b*rho;
-//		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
-//		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
-//		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
-//		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
-//		cv::line(srcMat, pt1, pt2, cv::Scalar(255, 0, 0), 1, CV_AA);
-//	}
-//	imshow("src", srcMat);
-//	imshow("dst", dstMat);
-//	waitKey(0);
-//}
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat srcMat = imread("G:\\picture\\18.jpg",0);
+	if (srcMat.empty())return -1;
+	Mat dstMat;
+	
+	std::vector<cv::Vec2f>lines;
+	Canny(srcMat, dstMat, 20, 230, 3, false);
+	
+	HoughLines(dstMat, lines,1,CV_PI/180,100);
+	std::vector<cv::Vec2f>::iterator it = lines.begin();
+	for (; it != lines.end(); ++it) {
+		float rho = (*it)[0], theta = (*it)[1];
+		Point pt1, pt2;
+		double a = cos(theta);
+		double b = sin(theta);
+		double x0 = a*rho;
+		double y0 = b*rho;
+		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
+		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
+		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
+		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
+		cv::line(srcMat, pt1, pt2, cv::Scalar(255, 0, 0), 1, CV_AA);
+	}
+	imshow("src", srcMat);
+	imshow("dst", dstMat);
+	waitKey(0);
+}
